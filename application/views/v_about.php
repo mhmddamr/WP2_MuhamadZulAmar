67fc0e78416bece96898b1d17ed6ce748b9c4f18
mhmddamr <amatrulaind@gmail.com>
2021-09-27 19:48:09 +0700

    pertemuan4

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Muhamad Zul</li>
+        <li>Nama Belakang : Amar</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Harapan 2 Kelapa Gading Barat </li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Berenang</li>
+        <li>Futsal</li>
+    </ul>
+</section>
